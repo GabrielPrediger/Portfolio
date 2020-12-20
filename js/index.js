4511eb7111426be781401f68d807e8befab16b7c
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2020-12-20 16:13:41 -0300

    v01
    Adição
    + responsividade DESKTOP e MOBILE
    + estilo footer
    + modal

---
+++ js/index.js
@@ -0,0 +1,37 @@
+var modal = document.querySelector("#modalOverlay")
+
+
+var projetos = document.querySelectorAll(".imagem-projeto-faculdade")
+
+for(let projeto of projetos){
+    projeto.addEventListener("click", function(){
+        const videoId = projeto.getAttribute("id");
+        modal.querySelector("iframe").src=`https://www.youtube.com/embed/${videoId}`
+        openModal();
+    })
+}
+
+
+
+function openModal(){
+    modal.classList.add("openModal")
+    var whiteModal = document.querySelector(".modal")
+    setTimeout(function(){
+        modal.classList.add("defModal");
+        modal.classList.remove("modalOverlay");
+        modal.classList.remove("openModal");
+
+}, 2100);
+    setTimeout(function(){whiteModal.classList.add("whiteModal")}, 1500);
+}
+
+
+function closeModal(){
+
+        modal.classList.remove("defModal");
+        modal.classList.add("modalOverlay");
+        whiteModal.classList.remove("whiteModal")
+        document.querySelector("#whiteModal").classList.remove("whiteModal")
+        modal.querySelector("iframe").src=""
+
+}
